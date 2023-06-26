a8727042a43e2965eff15c232b61116abb3f3442
kor_ka <badgrib@gmail.com>
2023-06-26 22:12:55 +0400

    fix(server): tg - migrate description -> title

--- server/src/api/tg/renderPin.ts
+++ server/src/api/tg/renderPin.ts
@@ -18,10 +18,10 @@ export const renderPin = async (chatId: number, threadId: number | undefined, ev
   const chatMetaModule = container.resolve(ChatMetaModule);
   const timeZones = new Set<string>();
   events.forEach(e => timeZones.add(e.tz));
-  const lines = events.map(({ date, tz, description, }) => {
+  const lines = events.map(({ date, tz, title, description, }) => {
     const dateStr = new Date(date).toLocaleString('en', { month: 'short', day: 'numeric', timeZone: tz })
     const timeStr = new Date(date).toLocaleTimeString('en', { hour: '2-digit', minute: '2-digit', hourCycle: 'h24', timeZone: tz })
-    return `🗓️ ${dateStr} - ${description}, ${timeStr} ${timeZones.size > 1 ? `(${tz})` : ''}`;
+    return `🗓️ ${dateStr} - ${title}, ${timeStr} ${timeZones.size > 1 ? `(${tz})` : ''}`;
   });
 
   let key = [chatId, threadId].filter(Boolean).join('_');
