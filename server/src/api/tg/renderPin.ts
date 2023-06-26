43292fd46b63660353089e859ad19400ef22c595
kor_ka <badgrib@gmail.com>
2023-06-26 23:57:14 +0400

    chore(server): tg pin - line before ics

--- server/src/api/tg/renderPin.ts
+++ server/src/api/tg/renderPin.ts
@@ -30,9 +30,9 @@ export const renderPin = async (chatId: number, threadId: number | undefined, ev
 
   let key = [chatId, threadId].filter(Boolean).join('_');
   const token = getChatToken(chatId);
-  key = [key, token].filter(Boolean).join('T')
+  key = [key, token].filter(Boolean).join('T');
 
-  lines.push(`<a href="https://tg-clndr-4023e1d4419a.herokuapp.com/ics/${key}/cal.ics">add to my calendar</a> ← tap to add or <a href="https://support.apple.com/guide/iphone/use-multiple-calendars-iph3d1110d4/ios#:~:text=Subscribe">subscribe</a>`);
+  lines.push(`\n<a href="https://tg-clndr-4023e1d4419a.herokuapp.com/ics/${key}/cal.ics">add to my calendar</a> ← tap to add or <a href="https://support.apple.com/guide/iphone/use-multiple-calendars-iph3d1110d4/ios#:~:text=Subscribe">subscribe</a>`);
 
   const text = lines.length > 1 ? lines.join('\n').trim() : '🗓️ no upcoming events';
 
