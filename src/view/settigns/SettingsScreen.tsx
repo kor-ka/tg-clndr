ee3b725d8e92ed79bd1e4845bee22873023b7d38
Gleb Putintsev <badgrib@gmail.com>
2023-10-15 02:17:53 +0400

    fix(client): settings main button controller

--- src/view/settigns/SettingsScreen.tsx
+++ src/view/settigns/SettingsScreen.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { ChatSettings } from "./ChatSettings";
 import { BackButtonController } from "../uikit/tg/BackButtonController";
+import { MainButtonController } from "../uikit/tg/MainButtonController";
 import { UserSettings } from "./UserSettings";
 
 export const SettingsScreen = () => {
+    const onClickStub = React.useCallback(() => {}, []);
     return <>
         <BackButtonController />
         <div style={{ display: 'flex', flexDirection: 'column', padding: '20px 0px' }}>
             <UserSettings />
             <ChatSettings />
         </div>
+        <MainButtonController onClick={onClickStub} isVisible={false} />
     </>
 }
