ea3556415019388d1802cda1278fe2448d90297a
Gleb Putintsev <badgrib@gmail.com>
2024-06-12 20:28:25 +0000

    chore(server): remove test error

--- server/src/instrument.js
+++ server/src/instrument.js
@@ -3,19 +3,3 @@ import * as Sentry from "@sentry/node";
 Sentry.init({
   dsn: "https://d04dc9e64c3fc6ba0227a878441357dc@o466449.ingest.us.sentry.io/4507420521922560",
 });
-
-Sentry.startSpan(
-  {
-    op: "test",
-    name: "My First Test Transaction",
-  },
-  () => {
-    setTimeout(() => {
-      try {
-        foo();
-      } catch (e) {
-        Sentry.captureException(e);
-      }
-    }, 99);
-  },
-);
