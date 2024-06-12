e19dd2ebfef4a7fd9eb3fb5b8177e7d4e96b71c9
Gleb Putintsev <badgrib@gmail.com>
2024-06-12 15:13:06 +0000

    chore(server): test error

--- server/src/instrument.js
+++ server/src/instrument.js
@@ -3,3 +3,19 @@ import * as Sentry from "@sentry/node";
 Sentry.init({
   dsn: "https://2f3707817f9746988922c71086a9765c@o466449.ingest.us.sentry.io/5480776",
 });
+
+Sentry.startSpan(
+  {
+    op: "test",
+    name: "My First Test Transaction",
+  },
+  () => {
+    setTimeout(() => {
+      try {
+        foo();
+      } catch (e) {
+        Sentry.captureException(e);
+      }
+    }, 99);
+  },
+);
