43ab85a2f0bb30a8a51fd7f391a192a7177a9572
Gleb Putintsev <badgrib@gmail.com>
2024-06-13 18:23:08 +0000

    fix(server): mesure - catch block

--- server/src/utils/mesure.ts
+++ server/src/utils/mesure.ts
@@ -3,6 +3,6 @@ export const mesure = <T>(factory: () => Promise<T>, tag: string) => {
     const promise = factory()
     promise.then(() => {
         console.log(tag, Date.now() - time)
-    })
+    }).catch(e => console.log(e))
     return promise
 }
